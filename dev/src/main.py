eb972781db2b3271b43c5d682d2bbe2ee4e5ec60
Adriano <adrianocardoso1991@gmail.com>
2024-07-31 23:45:39 -0300

    Feat: created bigquery connection using python libraries

--- dev/src/main.py
+++ dev/src/main.py
@@ -1,2 +1,20 @@
-def main():
-    return "test"
+from google.cloud import bigquery
+from google.auth import load_credentials_from_file
+
+cred, projectId = load_credentials_from_file("dev/src/crypto-ethereum-bigquery-key.json")
+
+client = bigquery.Client(project=projectId, credentials=cred)
+
+query = """
+    SELECT
+        table_name, ddl
+    FROM
+        `bigquery-public-data`.crypto_ethereum.INFORMATION_SCHEMA.TABLES
+    WHERE
+        table_name = 'tokens';
+"""
+
+results = client.query(query)
+
+for row in results:
+    print(row)
