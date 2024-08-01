ebf232ff64ff9cb1c5ade6a7e1265b0aa652fbde
Adriano <adrianocardoso1991@gmail.com>
2024-08-01 00:07:46 -0300

    Feat: deployed postgresql database to docker using terraform

--- dev/src/terraform/main.tf
+++ dev/src/terraform/main.tf
@@ -1,6 +1,18 @@
-provider "local" {}
+# Creates local postgres instance to persist ethereum data
+resource "docker_image" "postgres" {
+  name = var.postgres_image
+}
 
-resource "local_file" "test" {
-  filename = "test.txt"
-  content  = "test file"
+resource "docker_container" "postgres" {
+  name  = var.container_name
+  image = docker_image.postgres.name
+  ports {
+    internal = var.container_port
+    external = var.host_port
+  }
+  env = [
+    "POSTGRES_USER=${var.postgres_user}",
+    "POSTGRES_PASSWORD=${var.postgres_password}",
+    "POSTGRES_DB=${var.postgres_db}"
+  ]
 }
