ed177c01653b5a49985eb043fbed2ab330900fee
Adriano <adrianocardoso1991@gmail.com>
2024-08-01 00:47:12 -0300

    Feat: created files to test postgres connection

--- STEPS.md
+++ STEPS.md
@@ -14,6 +14,7 @@ do Airflow em um repositório git;
 chegando no banco Postgres
 
 # STEP BY STEP
+## DEV
 ### Gerar json de autenticação, ou via cli ou baixando o arquivo via interface web
 1. Ter o gcloud cli instalado
 1. Checar se está autenticado 
@@ -32,3 +33,10 @@ chegando no banco Postgres
     export GOOGLE_APPLICATION_CREDENTIALS=~/key.json
 1. Adicionar as roles no IAM para acesso aos dados no Bigquery
     gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:python-bigquery-sa@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/bigquery.user"
+
+### Criar instância postgres no Docker usando terraform
+1. Executar terraform apply no ambiente de dev, pasta terraform. *** dados de autenticação salvos no próprio arquivo de variables ***
+
+### Criar tabela no postgres
+1. Utilizar psycopg2 para conexão no postgres 
+1. Utilizar dll gerado pela query ao "bigquery-public-data.crypto_ethereum.INFORMATION_SCHEMA.TABLES" para criar a tabela
--- dev/src/main.py
+++ dev/src/main.py
@@ -1,20 +1,52 @@
+import psycopg2
 from google.cloud import bigquery
 from google.auth import load_credentials_from_file
 
-cred, projectId = load_credentials_from_file("dev/src/crypto-ethereum-bigquery-key.json")
 
-client = bigquery.Client(project=projectId, credentials=cred)
+def read_bigquery():
+    cred, projectId = load_credentials_from_file("dev/src/crypto-ethereum-bigquery-key.json")
 
-query = """
-    SELECT
-        table_name, ddl
-    FROM
-        `bigquery-public-data`.crypto_ethereum.INFORMATION_SCHEMA.TABLES
-    WHERE
-        table_name = 'tokens';
-"""
+    client = bigquery.Client(project=projectId, credentials=cred)
 
-results = client.query(query)
+    query = """
+        SELECT
+            table_name, ddl
+        FROM
+            `bigquery-public-data`.crypto_ethereum.INFORMATION_SCHEMA.TABLES
+        WHERE
+            table_name = 'tokens';
+    """
 
-for row in results:
-    print(row)
+    results = client.query(query)
+
+    for row in results:
+        print(row)
+
+def access_postgres():
+    db_host = "localhost"
+    db_database = "crypto_ethereum"
+    db_user = "root"
+    db_password = "password"
+
+    try:
+        with psycopg2.connect(host=db_host, database=db_database, user=db_user, password=db_password) as conn:
+            print("Connected to the postgres server")
+            return conn
+        
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def query_postgres():
+
+    with access_postgres() as conn:
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT datname FROM pg_database;")
+        print(cur.fetchall())
+
+        cur.close()
+
+if __name__ == "__main__":
+    read_bigquery()
+    query_postgres()
