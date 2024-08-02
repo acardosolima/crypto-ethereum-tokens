4173fd01d8f20d3f6fee8926a7ba7cb3194cb159
Adriano <adrianocardoso1991@gmail.com>
2024-08-02 15:27:26 -0300

    Chore: reogarnized terraform code structure

--- dev/src/terraform/postgres/main.tf
+++ dev/src/terraform/postgres/main.tf
@@ -6,10 +6,12 @@ resource "docker_image" "postgres" {
 resource "docker_container" "postgres" {
   name  = var.container_name
   image = docker_image.postgres.name
+
   ports {
     internal = var.container_port
     external = var.host_port
   }
+
   env = [
     "POSTGRES_USER=${var.postgres_user}",
     "POSTGRES_PASSWORD=${var.postgres_password}",
