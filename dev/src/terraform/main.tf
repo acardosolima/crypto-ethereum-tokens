44299c9efcce20cdfdc16cfe782a636d28740a10
Adriano <adrianocardoso1991@gmail.com>
2024-08-03 10:10:59 -0300

    Style: fixed terraform format

--- dev/src/terraform/main.tf
+++ dev/src/terraform/main.tf
@@ -1,13 +1,12 @@
 module "postgresql" {
-  source    = "./modules/postgres"
+  source = "./modules/postgres"
 }
 
-# --------get clusterIP ip address
-#module.postgresql.postgresql_cluster_ip
-# ----------get postgres user password
-#module.postgresql.postgresql_user_password
-# ---------create connection in airflow type postgres
+#get clusterIP ip address
+output "postgresql_cluster_ip" {
+  value = module.postgresql.postgresql_cluster_ip
+}
 
 module "airflow" {
-  source    = "./modules/airflow"
+  source = "./modules/airflow"
 }
