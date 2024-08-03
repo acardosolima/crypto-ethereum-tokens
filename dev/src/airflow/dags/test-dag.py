0b0119e0a4953a43daf9c12f8e851bd9770c163c
Adriano <adrianocardoso1991@gmail.com>
2024-08-03 12:01:30 -0300

    Feat: added dag to test gitsync on branch develop

--- dev/src/airflow/dags/test-dag.py
+++ dev/src/airflow/dags/test-dag.py
@@ -1,40 +1,44 @@
-from datetime import timedelta
+import pandas as pd
+from datetime import timedelta, datetime
 from airflow import DAG
-from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
 
 default_args = {
-    'owner': 'Adriano',
+    'owner': 'Data Engineer',
     'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': datetime(2024, 7, 20),
     'retries': 1,
-    'retry_delay': timedelta(minutes=3),
+    'retry_delay': timedelta(minutes=1),
 }
 
 dag = DAG(
-    'Crypto_Ethereum.Token',
+    'Ingest Tokens dataset',
+    description='Ingest data from bigquery-public-data.crypto_ethereum.tokens to PostgreSQL',
     default_args=default_args,
-    description='Gather sample data from bigquery-public-data.crypto_ethereum.tokens',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(1),
-    catchup=False,
+    schedule_interval='@daily',
+    catchup=True,
 )
 
 def query_tokens_table(**kwargs):
+    file_path = f"/tmp/query_results_{{ ds_nodash }}.csv"
+    
     hook = BigQueryHook(gcp_conn_id='bigquery_credentials', use_legacy_sql=True)
     conn = hook.get_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM [bigquery-public-data.crypto_ethereum.tokens] LIMIT 10")
-    result = cursor.fetchall()
-    return result
+    cursor.execute('SELECT * FROM [bigquery-public-data.crypto_ethereum.tokens] WHERE block_timestamp > "{{ ds_nodash }}-01 00:00:00"')
+    rows = cursor.fetchall()
+  
+    df = pd.DataFrame(rows)
+    df.to_csv(file_path, index=False)
+    
+    return file_path
 
-start_task = PythonOperator(
+fetch_task = PythonOperator(
     task_id='query_data',
     python_callable=query_tokens_table,
     dag=dag,
 )
 
-start_task 
+fetch_task 
