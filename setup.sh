c550fa052bb8fa6eec2e2af043ff62db15555715
Adriano <adrianocardoso1991@gmail.com>
2024-08-04 19:42:30 -0300

    Feat: added logic to setup.sh to automatically start minikube based on current status

--- setup.sh
+++ setup.sh
@@ -3,14 +3,29 @@
 TERRAFORM_DIR="dev/src/terraform/"
 DESTROY_FLAG=$1
 
-#minikube start
+check_minikube(){
+    minikube_status=$(minikube status --format='{{.Host}}')
 
-if [ "$DESTROY_FLAG" == "destroy" ]; then
-    terraform -chdir=$TERRAFORM_DIR destroy --auto-approve
-fi
+    if [[ "$minikube_status" != "Running"]]; then
+        minikube start
 
-terraform -chdir=$TERRAFORM_DIR init 
-terraform -chdir=$TERRAFORM_DIR validate 
-terraform -chdir=$TERRAFORM_DIR apply -auto-approve
+        if [[$? -ne 0]]; then
+            exit 1
+        fi
+    fi
+}
 
-#export POSTGRES_PASSWORD=$(kubectl get secret --namespace postgresql postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
+execute_terraform(){
+    if [ "$DESTROY_FLAG" == "destroy" ]; then
+        terraform -chdir=$TERRAFORM_DIR destroy --auto-approve
+    fi
+
+    terraform -chdir=$TERRAFORM_DIR init 
+    terraform -chdir=$TERRAFORM_DIR validate 
+    terraform -chdir=$TERRAFORM_DIR apply -auto-approve
+}
+
+check_minikube
+execute_terraform
+
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace postgresql postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
