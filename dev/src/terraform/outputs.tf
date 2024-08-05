ac34d235e7ab7b0489f5b9c6ea334342d79448ca
Adriano <adrianocardoso1991@gmail.com>
2024-08-05 07:59:10 -0300

    Docs: added comments to terraform outputs file

--- dev/src/terraform/outputs.tf
+++ dev/src/terraform/outputs.tf
@@ -3,6 +3,7 @@ output "postgresql_ip" {
   value = module.postgresql.postgresql_cluster_ip
 }
 
+# PostgreSQL secrets (can be used to automatically setup the Airflow connection)
 output "secret_keys" {
   value     = module.postgresql.secret_keys
   sensitive = true
