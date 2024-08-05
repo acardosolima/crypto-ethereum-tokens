58d5814dfb962046f7db6d085efd9d4fee9c5000
Adriano <adrianocardoso1991@gmail.com>
2024-08-05 07:46:12 -0300

    Docs: added comments to bigquery-dag file

--- dev/src/airflow/dags/bigquery-dag.py
+++ dev/src/airflow/dags/bigquery-dag.py
@@ -18,35 +18,43 @@ dag = DAG(
     retry_delay= timedelta(minutes=5)
 )
 
-def query_tokens_table(**kwargs):    
+def query_tokens_table(**kwargs):
+
+    # Creates starting and ending dates for the SQL query.
+    # Parses date format to be consistent with BigQuery table    
     execution_date = kwargs['data_interval_start'] 
     beg_date = execution_date.replace(hour=0, minute=0, second=0, microsecond=0)
     end_date = beg_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
 
-    print(beg_date)
-    print(end_date)
-    
+    # Defines a path to host temporarily the data download from the remote host
     file_path = f"/tmp/query_results_{execution_date.strftime('%Y%m%d%H%M')}.csv"
 
+    # Creates the connection and executes the select query
     hook = BigQueryHook(gcp_conn_id='bigquery_credentials', use_legacy_sql=True)
     conn = hook.get_conn()
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM [bigquery-public-data.crypto_ethereum.tokens] WHERE block_timestamp >= '{beg_date}' and block_timestamp < '{end_date}';")
     rows = cursor.fetchall()
     
+    # Stores information as CSV on the worker's tmp folder
     df = DataFrame(rows)
     df.to_csv(file_path, index=False, header=False)
 
     return file_path
 
+
 def save_to_postgres(**kwargs):
+
+    # Uses information from query_tokens_table to read the CSV file
     ti = kwargs['ti']
     file_path = ti.xcom_pull(task_ids='query_bigquery_task')
     
+    # Generates client to connect to postgres
     pg_hook = PostgresHook(postgres_conn_id='postgres_credentials')
     pg_conn = pg_hook.get_conn()
     cursor = pg_conn.cursor()
 
+    # Sends the whole file as a insert into statement, since its schema follows exactly what is on Postgres
     copy_sql = f"""
     COPY tokens(address, symbol, name, decimals,total_supply,block_timestamp,block_number,block_hash)
     FROM STDIN WITH (FORMAT CSV, DELIMITER ',');
@@ -63,6 +71,7 @@ def save_to_postgres(**kwargs):
 fetch_task = PythonOperator(
     task_id='query_bigquery_task',
     python_callable=query_tokens_table,
+    provide_context=True,
     dag=dag,
 )
 
