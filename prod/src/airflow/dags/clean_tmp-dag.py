e628d6ffa82a3d296e096f9b1549ed1f6364d3d6
Adriano Lima <adrianocardoso1991@gmail.com>
2024-08-05 12:06:12 -0300

    Feat/create release (#19)
    * Fix: default_args in wrong location for dags files
    
    * Fix: changed params of dag init
    
    * Feat/create release (#16)
    
    * Docs: fixed airflow connections formatting
    
    * Feat: shifted configuration to prod environment
    
    * Fix: fixed main.tf root file
    
    * Docs: added helm installation to required programs in README
    
    * Chore: changed flake8 workflow executor
    
    * Fix: reversed linter executor
    
    * Test: disabled main.py testing since it's not used
    
    * Fix: default_args in wrong location for dags files
    
    * Fix: changed params of dag init
    
    * Fix: added missing comma in dag definition
    
    * Fix: adjusted duplicated prod dag files

--- prod/src/airflow/dags/clean_tmp-dag.py
+++ prod/src/airflow/dags/clean_tmp-dag.py
@@ -2,16 +2,19 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
+default_args = {
+    "owner": "Data Engineer",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
 
 dag = DAG(
     'Clean-Temp-folder',
-    owner= 'Data Engineer',
     description='Clean temp folder used to persist data retrieved from BigQuery',
     schedule_interval=timedelta(weeks=1),
     start_date=datetime(2024, 8, 5),
     catchup=False,
-    retries= 1,
-    retry_delay= timedelta(minutes=5)
+    default_args=default_args
 )
 
 clean_temp_folder = BashOperator(
